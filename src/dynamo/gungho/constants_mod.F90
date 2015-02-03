1dc96233af92941cc61462ebefbce3200b2b8f3e
tommasobenacchio <enquiries@metoffice.gov.uk>
2015-02-03 14:12:11 +0000

    ticket:187 for Steve Mullerworth: improve comments in constants_mod.
    git-svn-id: https://metomi/svn/lfric.xm/Dynamo/trunk@2356 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- src/dynamo/gungho/constants_mod.F90
+++ src/dynamo/gungho/constants_mod.F90
@@ -5,7 +5,7 @@
 ! whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
 !-------------------------------------------------------------------------------
 !
-!> @brief Define various constants.
+!> @brief Define various constants for the application.
 !>
 !> @details Various computational, physical and geometrical constants are
 !>          defined in this module. Their values are also set here.
@@ -17,14 +17,14 @@ module constants_mod
 
   ! Define default application-defined kinds for all intrinsic data types
 
-  !> @name Reals
+  !> @name Set up default kinds for real and double-precision variables.
   !> @{
-  real,             private :: r_val
-  double precision, private :: dp_val
+  real,             private :: r_val  !< A native real used to compute kind of native real.
+  double precision, private :: dp_val !< A native double-precision used to compute kind of native dp.
 
   integer, parameter :: r_def     = real64 !< Default real kind for application.
-  integer, parameter :: r_single  = real32 !< Single precision real kind.
-  integer, parameter :: r_double  = real64 !< Double precesion real kind.
+  integer, parameter :: r_single  = real32 !< Default single precision real kind for application.
+  integer, parameter :: r_double  = real64 !< Default double precision real kind for application.
 
   integer, parameter :: r_native  = kind(r_val)  !< Native kind for real.
   integer, parameter :: dp_native = kind(dp_val) !< Native kind for double precision.
@@ -34,80 +34,92 @@ module constants_mod
   !> @{
   !> @}
 
-  !> @name Integers
+  !> @name Set up default kinds for integers.
   !> @{
-  integer, private   :: i_val
+  integer, private   :: i_val     !< A native integer used to compute kind of native integer.
 
-  integer, parameter :: i_def     = int32       !< Default integer kind.
+  integer, parameter :: i_def     = int32       !< Default integer kind for application.
   integer, parameter :: i_native  = kind(i_val) !< Native kind for integer.
   !> @}
 
-  !> @name Logical
+  !> @name Set up default kinds for logicals.
   !> @{
-  logical, private   :: l_val
+  logical, private   :: l_val     !< A native logical used to compute kind of native logical.
 
-  integer, parameter :: l_def     = kind(l_val) !< Default logical kind.
+  integer, parameter :: l_def     = kind(l_val) !< Default logical kind for application.
   integer, parameter :: l_native  = kind(l_val) !< Native kind for logical.
   !> @}
 
-  !> @name Character
+  !> @name Set up default kinds for character variables.
   !> @{
-  character, private :: c_val
+  character, private :: c_val     !< A native character used to compute kind of native character.
 
-  integer, parameter :: c_def     = kind(c_val) !< Default character kind.
+  integer, parameter :: c_def     = kind(c_val) !< Default character kind for application.
   integer, parameter :: c_native  = kind(c_val) !< Native kind for character.
   !> @}
 
-  !> String Length
+  !> @name Set up default lengths for string variables.
   !> @{
-  integer, parameter :: str_def          = 128 !< Default string length.
-  integer, parameter :: str_long         = 255
-  integer, parameter :: str_max_filename = 255
+  integer, parameter :: str_def          = 128 !< Default string length for normal strings.
+  integer, parameter :: str_long         = 255 !< Default length of long string.
+  integer, parameter :: str_max_filename = 255 !< Default maximum length of a file-name.
   !> @}
 
-  !> Platform constants
+  !> @name Platform constants
   !> @{
-  real(kind=r_def), parameter :: LARGE_REAL = huge(0.0_r_def)
+  real(kind=r_def), parameter :: LARGE_REAL = huge(0.0_r_def) !< The largest number of kind r_def that is not an infinity.
   !> @}
 
-  !> Numerical constants
+  !> @name Numerical constants
   !> @{
-  real(kind=r_def), parameter :: PI  = 3.141592654_r_def !< pi value
-  real(kind=r_def), parameter :: EPS = 3.0e-15_r_def     !< relative precision
+  real(kind=r_def), parameter :: EPS = 3.0e-15_r_def !< Relative precision: if (abs(x-y) > EPS) then assume x==y.
+  !> @}
+
+  !> @name Mathematical constants
+  !> @{
+  real(kind=r_def), parameter :: PI  = 3.141592654_r_def !< Value of pi.
   !> @}
 
   !> @name Physical constants
   !> @{
-  real(kind=r_def), parameter :: GRAVITY = 9.80665_r_def !< Acceleration due to gravity.
-  real(kind=r_def), parameter :: Rd = 287.05_r_def       !< rd
-  real(kind=r_def), parameter :: Cp = 1005.0_r_def       !< cp
-  real(kind=r_def), parameter :: KAPPA = 287.05_r_def/1005.0_r_def !< kappa
-  real(kind=r_def), parameter :: P_ZERO = 100000.0_r_def !< p0
-  real(kind=r_def), parameter :: N_SQ = 0.0001_r_def     !< n^2
-  real(kind=r_def), parameter :: OMEGA_UNSCALED = 7.292116E-5_r_def !< omega
-  real(kind=r_def), parameter :: EARTH_RADIUS_UNSCALED = 6371229.0_r_def !< Radius of the Earth in meters.
+  real(kind=r_def), parameter :: GRAVITY = 9.80665_r_def !< Surface equatorial value of gravity [m/s^2].
+  real(kind=r_def), parameter :: Rd = 287.05_r_def       !< Gas constant for dry air [J/(kg K)].
+  real(kind=r_def), parameter :: Cp = 1005.0_r_def       !< Specific heat of dry air at constant pressure [J/(kg K)].
+  real(kind=r_def), parameter :: KAPPA = Rd/Cp           !< Ratio of Rd and Cp [dimensionless].
+  real(kind=r_def), parameter :: P_ZERO = 100000.0_r_def !< Reference surface pressure [Pa].
+  real(kind=r_def), parameter :: N_SQ = 0.0001_r_def     !< The square of Brunt-Vaisala frequency [1/s^2].
+  real(kind=r_def), parameter :: OMEGA_UNSCALED = 7.292116E-5_r_def !< Angular velocity [rad/s].
+  real(kind=r_def), parameter :: EARTH_RADIUS_UNSCALED = 6371229.0_r_def !< Radius of the Earth [m].
   !> @}
 
-  !> @name Small earth scalings
+  !> @name Small Earth scalings
   !> @{
-  real(kind=r_def), parameter :: EARTH_SCALING = 125.0_r_def
-  real(kind=r_def)            :: omega = OMEGA_UNSCALED*EARTH_SCALING*0.0_r_def
-  real(kind=r_def)            :: earth_radius = EARTH_RADIUS_UNSCALED/EARTH_SCALING
+  real(kind=r_def), parameter :: EARTH_SCALING = 125.0_r_def !< Scaling factor to modify Earth parameters.
+  real(kind=r_def)            :: omega = OMEGA_UNSCALED*EARTH_SCALING*0.0_r_def !< Scale up rotation [rad/s].
+  real(kind=r_def)            :: earth_radius = EARTH_RADIUS_UNSCALED/EARTH_SCALING !< Scale down Earth radius [m].
   !> @}
 
-  ! Linear solver constants
-  integer (kind=i_def), parameter :: MAX_ITER = 99 !< maximum iteration number for solver
-
-  integer (kind=i_def), parameter :: CG_SOLVER     = 1, &
-                                     BICG_SOLVER   = 2, &
-                                     JACOBI_SOLVER = 3, &
-                                     GMRES_SOLVER  = 4, &
-                                     GCR_SOLVER    = 5
-  integer (kind=i_def), parameter :: SOLVER_OPTION = BICG_SOLVER
-  integer (kind=i_def), parameter :: NO_PRE_COND       = -1, &
-                                     DIAGONAL_PRE_COND = 1
-  real(kind=r_def),     parameter :: SOLVER_TOL = 1.0e-4_r_def
-  integer (kind=i_def), parameter :: GCRK  = 4
+  !> @name Enumeration of the available choices for the linear solver 
+  !> @{
+  integer (kind=i_def), parameter :: CG_SOLVER     = 1 !< Conjugate Gradient solver option.
+  integer (kind=i_def), parameter :: BICG_SOLVER   = 2 !< BiCGSTAB solver option.
+  integer (kind=i_def), parameter :: JACOBI_SOLVER = 3 !< Jacobi solver option.
+  integer (kind=i_def), parameter :: GMRES_SOLVER  = 4 !< Generalized Minimal RESidual solver option.
+  integer (kind=i_def), parameter :: GCR_SOLVER    = 5 !< Generalized Conjugate Residual solver option.
+  !> @}
+  !> @name Linear solver constants
+  !> @{
+  integer (kind=i_def), parameter :: MAX_ITER = 99 !< Maximum iteration number for solver.
+  integer (kind=i_def), parameter :: SOLVER_OPTION = BICG_SOLVER !< Choice of solver from the above list
+                                                                 !< (currently hard-wired).
+  integer (kind=i_def), parameter :: NO_PRE_COND       = -1 !< No preconditioner option.
+  integer (kind=i_def), parameter :: DIAGONAL_PRE_COND = 1  !< Diagonal preconditioner option.
+  real(kind=r_def),     parameter :: SOLVER_TOL = 1.0e-4_r_def !< Relative tolerance of solver.
+  integer (kind=i_def), parameter :: GCRK  = 4                 !< Dimension of the approximate Krylov subspace.
+                                                               !< In other words, it is the number of potential 
+                                                               !< residual vectors to calculate at each iteration 
+                                                               !< of the solver
+ !> @}
 
 end module constants_mod
 
