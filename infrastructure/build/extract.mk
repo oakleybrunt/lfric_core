12011557bd7cb1888a2d92eb5e4794ac99983566
samanthaadams <enquiries@metoffice.gov.uk>
2017-12-05 08:50:30 +0000

    ticket:1286 for Matthew Hambley. Don't extract program objects when importing GungHo source to a project.
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@12127 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- infrastructure/build/extract.mk
+++ infrastructure/build/extract.mk
@@ -6,9 +6,18 @@
 #
 # Run this make file to copy a source tree from SOURCE_DIR to WORKING_DIR
 #
+# Set WITHOUT_PROGRAMS in order to prevent files containing program program
+# units from being extracted.
+#
 .PHONY: files-to-extract
+ifdef WITHOUT_PROGRAMS
+MODULE_FILES = $(shell find $(SOURCE_DIR) -path "$(SOURCE_DIR)/*/*" -name '*.[Ff]90' -printf '%P\n')
+files-to-extract: $(addprefix $(WORKING_DIR)/,$(MODULE_FILES))
+	$(Q)echo >/dev/null
+else
 files-to-extract: $(addprefix $(WORKING_DIR)/,$(shell find $(SOURCE_DIR) -name '*.[Ff]90' -printf '%P\n'))
 	$(Q)echo >/dev/null
+endif
 
 .PRECIOUS: $(WORKING_DIR)/%.F90
 $(WORKING_DIR)/%.F90: $(SOURCE_DIR)/%.F90 | $(WORKING_DIR)
