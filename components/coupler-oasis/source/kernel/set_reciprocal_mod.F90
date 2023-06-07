87ab240cc255a091ca35e5322d6f528effb189ee
mikehobson <enquiries@metoffice.gov.uk>
2023-06-07 07:51:39 +0000

    ticket:3858 for Tom Bendall: Implement set_reciprocal kernel for multidata fields.
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@43162 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- components/coupler-oasis/source/kernel/set_reciprocal_mod.F90
+++ components/coupler-oasis/source/kernel/set_reciprocal_mod.F90
@@ -9,9 +9,10 @@
 module set_reciprocal_mod
 
 use kernel_mod,    only : kernel_type
-use argument_mod,  only : arg_type,            &
-                          GH_REAL,GH_FIELD,    &
-                          GH_WRITE, GH_READ,   &
+use argument_mod,  only : arg_type,                  &
+                          GH_REAL, GH_FIELD,         &
+                          GH_SCALAR, GH_INTEGER,     &
+                          GH_WRITE, GH_READ,         &
                           ANY_DISCONTINUOUS_SPACE_1, &
                           CELL_COLUMN
 use constants_mod, only : r_def, i_def
@@ -26,9 +27,10 @@ private
 !> The type declaration for the kernel. Contains the metadata needed by the Psy layer
 type, public, extends(kernel_type) :: set_reciprocal_type
   private
-  type(arg_type) :: meta_args(2) = (/                                    &
-       arg_type(GH_FIELD, GH_REAL, GH_WRITE, ANY_DISCONTINUOUS_SPACE_1), &
-       arg_type(GH_FIELD, GH_REAL, GH_READ,  ANY_DISCONTINUOUS_SPACE_1)  &
+  type(arg_type) :: meta_args(3) = (/                                        &
+       arg_type(GH_FIELD,  GH_REAL,    GH_WRITE, ANY_DISCONTINUOUS_SPACE_1), &
+       arg_type(GH_FIELD,  GH_REAL,    GH_READ,  ANY_DISCONTINUOUS_SPACE_1), &
+       arg_type(GH_SCALAR, GH_INTEGER, GH_READ)                              &
        /)
   integer :: operates_on = CELL_COLUMN
 contains
@@ -46,12 +48,14 @@ contains
 !! @param[in]     nlayers          Number of layers
 !! @param[in,out] field_reciprocal Field to write the reciprocal to
 !! @param[in]     input_field      Input field to read from
+!! @param[in]     ndata            Number of data points per DoF
 !! @param[in]     ndf              Number of degrees of freedom per cell
 !! @param[in]     undf             Total number of unique degrees of freedom
 !! @param[in]     map              Dofmap for the cell at the base of the column
 
 subroutine set_reciprocal_code(nlayers,                       &
                                field_reciprocal, input_field, &
+                               ndata,                         &
                                ndf, undf, map)
 
   implicit none
@@ -60,20 +64,23 @@ subroutine set_reciprocal_code(nlayers,                       &
   integer(kind=i_def),                  intent(in)    :: nlayers
   integer(kind=i_def),                  intent(in)    :: ndf
   integer(kind=i_def),                  intent(in)    :: undf
+  integer(kind=i_def),                  intent(in)    :: ndata
   integer(kind=i_def), dimension(ndf),  intent(in)    :: map
   real(kind=r_def),    dimension(undf), intent(inout) :: field_reciprocal
   real(kind=r_def),    dimension(undf), intent(in)    :: input_field
 
   ! Internal variables
-  integer(kind=i_def) :: k, df
+  integer(kind=i_def) :: k, df, j
 
   do k = 0, nlayers - 1
     do df = 1, ndf
-      if (abs(input_field(map(df) + k)) > tiny(1.0_r_def)) then
-        field_reciprocal(map(df) + k) = 1.0_r_def/input_field(map(df) + k)
-      else
-        field_reciprocal(map(df) + k) = 0.0_r_def
-      end if
+      do j = 1, ndata
+        if (abs(input_field(map(df) + j - 1 + k)) > tiny(1.0_r_def)) then
+          field_reciprocal(map(df) + j - 1 + k) = 1.0_r_def/input_field(map(df) + j - 1 + k)
+        else
+          field_reciprocal(map(df) + j - 1 + k) = 0.0_r_def
+        end if
+      end do
     end do
   end do
 
