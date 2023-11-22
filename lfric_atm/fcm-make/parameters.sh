921a5ca20cb289702dd647add67feaede25aad62
giorgialine <enquiries@metoffice.gov.uk>
2023-11-22 23:47:19 +0000

    ticket:3640 for maggiehendry: Update LFRic coupled miniapp Rivers app to be consistent with Rivers-only dumps
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@46985 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- lfric_atm/fcm-make/parameters.sh
+++ lfric_atm/fcm-make/parameters.sh
@@ -22,7 +22,7 @@ export casim_sources=
 # miniapps/lfric_coupled/rose-stem/app/fcm_make_river/rose-app.conf
 # is consistent with jules_rev below.
 #******************************************************************************
-export jules_rev=um13.4
+export jules_rev=27111
 export jules_sources=
 export shumlib_rev=um13.4
 export shumlib_sources=
@@ -30,7 +30,7 @@ export socrates_rev=um13.4
 export socrates_sources=
 export ukca_rev=um13.4
 export ukca_sources=
-export um_rev=vn13.4
+export um_rev=121127
 export um_sources=
 
 #### Do not edit the definitions below this line without
