525fe85b3c918a735ea39b025a898f89a1047a5a
matthewhambley <enquiries@metoffice.gov.uk>
2025-01-24 14:46:11 +0000

    ticket:4524 for Edward Hone: Support nvfortran with mpif90 build.
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@51917 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- infrastructure/build/fortran/mpif90.mk
+++ infrastructure/build/fortran/mpif90.mk
@@ -4,9 +4,9 @@
 # under which the code may be used.
 ##############################################################################
 
-MPIF90_COMPILER := $(shell $(FC) --version \
-                   | awk -F " " 'NR<2 { printf "%s", $$1 }')
-$(info ** Chosen MPI Fortran compiler "$(MPIF90_COMPILER)")
+MPIF90_VN_STR := $(shell $(FC) --version)
+MPIF90_COMPILER := $(shell echo "$(MPIF90_VN_STR)" | awk '{print $$1}')
+$(info ** Chosen MPI Fortran compiler: $(MPIF90_COMPILER))
 
 ifeq '$(MPIF90_COMPILER)' 'GNU'
   FORTRAN_COMPILER = gfortran
@@ -14,8 +14,10 @@ else ifeq '$(MPIF90_COMPILER)' 'ifort'
   FORTRAN_COMPILER = ifort
 else ifeq '$(MPIF90_COMPILER)' 'Cray'
   FORTRAN_COMPILER = crayftn
+else ifeq '$(MPIF90_COMPILER)' 'nvfortran'
+  FORTRAN_COMPILER = nvfortran
 else
-  $(error Unrecognised mpif90 compiler option: "$MPIF90_COMPILER")
+  $(error Unrecognised mpif90 compiler option: "$(MPIF90_COMPILER)")
 endif
 
 include $(LFRIC_BUILD)/fortran/$(FORTRAN_COMPILER).mk
